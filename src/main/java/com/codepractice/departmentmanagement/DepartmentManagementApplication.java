403d983b7ef3919e7cbbbea12516f6c12b1db571
Yogesh Yadav <119784141+mr-yogesh@users.noreply.github.com>
2022-12-07 00:28:50 +0530

    Comment added
    About starter page of Spring Boot Application.

--- src/main/java/com/codepractice/departmentmanagement/DepartmentManagementApplication.java
+++ src/main/java/com/codepractice/departmentmanagement/DepartmentManagementApplication.java
@@ -2,7 +2,7 @@ package com.codepractice.departmentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Here in this page, we have only base configuration
 @SpringBootApplication
 public class DepartmentManagementApplication {
 
